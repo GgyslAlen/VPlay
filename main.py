3ec973357974f49fead7c999d03b371a68dbbbad
alenovich123@gmail.com <2783200Aa>
2021-11-12 11:52:36 +0500

    -fix

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import sys
-
-from moviepy.editor import *
 import pygame
-from pygame import KEYDOWN, K_ESCAPE
+import cv2
 
 
 def play(filename, windowname):
-    pygame.display.set_caption(windowname)
-    monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
-    pygame.display.set_mode(monitor_size, pygame.FULLSCREEN)
-    print(monitor_size)
-    video = VideoFileClip(filename)
-    video.resize(monitor_size).preview()
-    while True:
+    import cv2
+    import pygame
+
+    cap = cv2.VideoCapture(filename)
+    success, img = cap.read()
+    shape = img.shape[1::-1]
+    wn = pygame.display.set_mode(shape)
+    clock = pygame.time.Clock()
+
+    while success:
+        clock.tick(60)
+        success, img = cap.read()
         for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+            if event.type == pygame.QUIT:
+                success = False
+        wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
         pygame.display.update()
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
-    file_name = r"/home/pi/test_64_64.mp4"
+    file_name = r"E:\\test_64_64.mp4"
     window_name = "window"
     play(file_name, window_name)
