ac6933d8b6faf391d2ebc4451caaecc2d33e0aad
alenovich123@gmail.com <2783200Aa>
2021-11-12 12:28:35 +0500

    -fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 import cv2
 
 
-def play(filename, windowname):
+def play(filename):
     cap = cv2.VideoCapture(filename)
     success, img = cap.read()
     wn = pygame.display.set_mode((64, 64), pygame.FULLSCREEN)
@@ -15,6 +15,9 @@ def play(filename, windowname):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 success = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    success = False
         wn.blit(pygame.image.frombuffer(img.tobytes(), (64, 64), "BGR"), (0, 0))
         pygame.display.update()
 
@@ -22,6 +25,6 @@ def play(filename, windowname):
 
 
 if __name__ == '__main__':
-    file_name = r"E:\\test_64_64.mp4"
+    file_name = r"/home/pi/test_64_64.mp4"
     window_name = "window"
-    play(file_name, window_name)
+    play(file_name)
